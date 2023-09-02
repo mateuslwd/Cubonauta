84d5d03a0a336a762ae5b3bd1754c2026825244a
Mateus <vipcelular1050@gmail.com>
2023-09-02 23:04:40 +0100

    PopUp Filter Interface

--- src/componentes/searchBar/searchBar.tsx
+++ src/componentes/searchBar/searchBar.tsx
@@ -1,15 +1,37 @@
-import './searchBar.css'
+import { useState } from 'react'
+import './searchBarClose.css'
+import './searchBarOpen.css'
+
+import Filter from './Filter'
 
 function SearchBar(){
 
+    function toggle(){
+
+        if(state == 'Close'){
+            setState('Open')
+        } else {
+            setState('Close')
+        }
+    
+    }
+
+    const [state, setState] = useState('Close')
+
     return(
-        <div className='searchBar'>
-            <div className='container'>
-                <input placeholder='Pesquise aqui'></input>
-                <div className='VL'></div>
-                <div className='icon'></div>
+        <>
+            <div className='search'>
+                <div className={'searchBar' + state}>
+                    <div className='container'>
+                        <input placeholder='Pesquise aqui'></input>
+                        <div className='VL'></div>
+                        <div onClick={toggle} className='icon'></div>
+                    </div>
+                </div>
+                <hr></hr>
+                {state === 'Open' && <Filter />}
             </div>
-        </div>
+        </>
     )
 
 }
